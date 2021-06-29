97e469803ed955edefeedd5e43d117811965b03e
yun8boo <yuma051521@gmail.com>
2021-06-29 23:45:47 +0900

    commit

---
+++ README.md
@@ -0,0 +1,1 @@
+#  Next.js + NextAuth.js + Prisma SampleApp
---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import ProctedPage from '../components/ProctedPage'
+
+const IndexPage = () => {
+  return (
+    <ProctedPage>
+      <p>サインインしてないとみれません</p>
+    </ProctedPage>
+  )
+}
+
+export default IndexPage
